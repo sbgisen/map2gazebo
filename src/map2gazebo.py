90b7d5b58ebccebb6a7696422d98c78d3c67494d
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2024-07-08 10:32:29 +0900

    Refactoring
    WIP
    
    
    Refactoring
    
    
    Refactoring

--- src/map2gazebo.py
+++ src/map2gazebo.py
@@ -81,11 +81,12 @@ class MapConverter(object):
             vertices_dict = {}
             num_points = len(contour)
             for i, point in enumerate(contour):
-                x, y = point[0]
-                v0 = coords_to_loc((x, y), metadata)
-                v1 = coords_to_loc((x, y + 1), metadata)
-                v2 = coords_to_loc((x + 1, y), metadata)
-                v3 = coords_to_loc((x + 1, y + 1), metadata)
+                x_img, y_img = point[0]
+                x, y, z = coords_img2world(point[0], metadata)
+                v0 = coords_img2world((x_img + 0.5, y_img + 0.5), metadata)
+                v1 = coords_img2world((x_img - 0.5, y_img + 0.5), metadata)
+                v2 = coords_img2world((x_img - 0.5, y_img - 0.5), metadata)
+                v3 = coords_img2world((x_img + 0.5, y_img - 0.5), metadata)
 
                 # Initialize wall flags
                 set_wall_x_plus = True
@@ -95,51 +96,49 @@ class MapConverter(object):
 
                 # Check next point
                 if i < num_points - 1:
-                    next_point = contour[i + 1][0]
-                    next_x, next_y = next_point
-                    vector_x = next_y - y
-                    vector_y = -(next_x - x)
+                    next_x, next_y, next_z = coords_img2world(contour[i + 1][0], metadata)
+                    vector_x = next_x - x
+                    vector_y = next_y - y
                     direction = atan2(vector_y, vector_x)
                     if -pi / 4 <= direction < pi / 4:
-                        set_wall_y_plus = False
-                    elif pi / 4 <= direction < 3 * pi / 4:
                         set_wall_x_plus = False
+                    elif pi / 4 <= direction < 3 * pi / 4:
+                        set_wall_y_plus = False
                     elif -3 * pi / 4 <= direction < -pi / 4:
-                        set_wall_x_minus = False
-                    else:
                         set_wall_y_minus = False
+                    else:
+                        set_wall_x_minus = False
 
                 # Check previous point
                 if i > 0:
-                    prev_point = contour[i - 1][0]
-                    prev_x, prev_y = prev_point
-                    vector_x = prev_y - y
-                    vector_y = -(prev_x - x)
+                    prev_x, prev_y, prev_z = coords_img2world(contour[i - 1][0], metadata)
+                    vector_x = prev_x - x
+                    vector_y = prev_y - y
                     direction = atan2(vector_y, vector_x)
                     if -pi / 4 <= direction < pi / 4:
-                        set_wall_y_plus = False
-                    elif pi / 4 <= direction < 3 * pi / 4:
                         set_wall_x_plus = False
+                    elif pi / 4 <= direction < 3 * pi / 4:
+                        set_wall_y_plus = False
                     elif -3 * pi / 4 <= direction < -pi / 4:
-                        set_wall_x_minus = False
-                    else:
                         set_wall_y_minus = False
+                    else:
+                        set_wall_x_minus = False
 
                 # Add faces based on flags
                 if set_wall_x_plus:
-                    self.add_face(v2, v3, v2 + height, v3 + height, np.array([0, 0, 0]), faces_set)
-                if set_wall_x_minus:
-                    self.add_face(v0, v1, v0 + height, v1 + height, np.array([0, 0, 0]), faces_set, reverse=True)
+                    self.add_face(v0 + height, v0, v1, v1 + height, np.array([0, 0, 0]), faces_set, reverse=True)
                 if set_wall_y_plus:
-                    self.add_face(v1, v3, v1 + height, v3 + height, np.array([0, 0, 0]), faces_set, reverse=True)
+                    self.add_face(v0 + height, v0, v3, v3 + height, np.array([0, 0, 0]), faces_set)
+                if set_wall_x_minus:
+                    self.add_face(v3 + height, v3, v2, v2 + height, np.array([0, 0, 0]), faces_set)
                 if set_wall_y_minus:
-                    self.add_face(v0, v2, v0 + height, v2 + height, np.array([0, 0, 0]), faces_set)
+                    self.add_face(v1 + height, v1, v2, v2 + height, np.array([0, 0, 0]), faces_set, reverse=True)
 
                 # Add roof and floor faces
-                faces_set.add((tuple(v0 + height), tuple(v2 + height), tuple(v1 + height)))
                 faces_set.add((tuple(v2 + height), tuple(v3 + height), tuple(v1 + height)))
-                faces_set.add((tuple(v0), tuple(v1), tuple(v2)))
+                faces_set.add((tuple(v3 + height), tuple(v0 + height), tuple(v1 + height)))
                 faces_set.add((tuple(v2), tuple(v1), tuple(v3)))
+                faces_set.add((tuple(v3), tuple(v1), tuple(v0)))
 
             vertices = set()
             faces = []
@@ -154,7 +153,8 @@ class MapConverter(object):
             vertices = np.array([list(vertex) for vertex in vertices_dict])
             faces = np.array(faces)
             mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
-            mesh.remove_duplicate_faces()
+            # mesh.remove_duplicate_faces()
+            # mesh.remove_degenerate_faces()
             meshes.append(mesh)
 
         return meshes
@@ -164,39 +164,39 @@ class MapConverter(object):
         vertices_dict = {}
         faces_set = set()
 
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                if image[y, x] < self.threshold:
+        for y_img in range(image.shape[0]):
+            for x_img in range(image.shape[1]):
+                if image[y_img, x_img] < self.threshold:
                     continue
-
-                v0 = coords_to_loc((x, y), metadata)
-                v1 = coords_to_loc((x, y + 1), metadata)
-                v2 = coords_to_loc((x + 1, y), metadata)
-                v3 = coords_to_loc((x + 1, y + 1), metadata)
+                x, y, z = coords_img2world((x_img, y_img), metadata)
+                v0 = coords_img2world((x_img + 0.5, y_img + 0.5), metadata)
+                v1 = coords_img2world((x_img - 0.5, y_img + 0.5), metadata)
+                v2 = coords_img2world((x_img - 0.5, y_img - 0.5), metadata)
+                v3 = coords_img2world((x_img + 0.5, y_img - 0.5), metadata)
 
                 # Initialize wall flags
-                set_wall_x_plus = y == 0 or image[y - 1, x] < self.threshold
-                set_wall_x_minus = y == image.shape[0] - 1 or image[y + 1, x] < self.threshold
-                set_wall_y_plus = x == image.shape[1] - 1 or image[y, x + 1] < self.threshold
-                set_wall_y_minus = x == 0 or image[y, x - 1] < self.threshold
+                set_wall_x_plus = y_img == 0 or image[y_img - 1, x_img] < self.threshold
+                set_wall_x_minus = y_img == image.shape[0] - 1 or image[y_img + 1, x_img] < self.threshold
+                set_wall_y_plus = x_img == image.shape[1] - 1 or image[y_img, x_img + 1] < self.threshold
+                set_wall_y_minus = x_img == 0 or image[y_img, x_img - 1] < self.threshold
 
                 # Add faces based on flags
                 if set_wall_x_plus:
-                    self.add_face(v0, v2, v0 + height, v2 + height, np.array([0, 0, 0]), faces_set)
+                    self.add_face(v3 + height, v3, v2, v2 + height, np.array([0, 0, 0]), faces_set)
                 if set_wall_y_plus:
-                    self.add_face(v2, v3, v2 + height, v3 + height, np.array([0, 0, 0]), faces_set)
+                    self.add_face(v0 + height, v0, v3, v3 + height, np.array([0, 0, 0]), faces_set)
                 if set_wall_x_minus:
-                    self.add_face(v1, v3, v1 + height, v3 + height, np.array([0, 0, 0]), faces_set, reverse=True)
+                    self.add_face(v0 + height, v0, v1, v1 + height, np.array([0, 0, 0]), faces_set, reverse=True)
                 if set_wall_y_minus:
-                    self.add_face(v0, v1, v0 + height, v1 + height, np.array([0, 0, 0]), faces_set, reverse=True)
+                    self.add_face(v1 + height, v1, v2, v2 + height, np.array([0, 0, 0]), faces_set, reverse=True)
 
                 # Roof face
-                faces_set.add((tuple(v0 + height), tuple(v2 + height), tuple(v1 + height)))
                 faces_set.add((tuple(v2 + height), tuple(v3 + height), tuple(v1 + height)))
+                faces_set.add((tuple(v3 + height), tuple(v0 + height), tuple(v1 + height)))
 
                 # Floor face
-                faces_set.add((tuple(v0), tuple(v1), tuple(v2)))
                 faces_set.add((tuple(v2), tuple(v1), tuple(v3)))
+                faces_set.add((tuple(v3), tuple(v1), tuple(v0)))
 
         vertices = set()
         faces = []
@@ -221,18 +221,18 @@ class MapConverter(object):
         v2 = tuple(v2 + height_offset)
         v3 = tuple(v3 + height_offset)
         if reverse:
+            faces_set.add((v0, v1, v3))
             faces_set.add((v3, v1, v2))
-            faces_set.add((v2, v1, v0))
         else:
-            faces_set.add((v0, v1, v2))
             faces_set.add((v2, v1, v3))
+            faces_set.add((v3, v1, v0))
 
 
-def coords_to_loc(coords, metadata):
-    x, y = coords
-    loc_x = x * metadata.resolution + metadata.origin.position.x
-    loc_y = y * metadata.resolution + metadata.origin.position.y
-    return np.array([loc_x, loc_y, 0.0])
+def coords_img2world(img_pixel, metadata):
+    x_img, y_img = img_pixel
+    x_world = ((y_img + 0.5) * metadata.resolution + metadata.origin.position.y)
+    y_world = -((x_img + 0.5) * metadata.resolution + metadata.origin.position.x)
+    return np.array([x_world, y_world, 0.0])
 
 
 def main():
@@ -240,7 +240,7 @@ def main():
     map_topic = rospy.get_param("~map_topic", "map")
     occupied_thresh = rospy.get_param("~occupied_thresh", 1)
     box_height = rospy.get_param("~box_height", 2.0)
-    use_contours = rospy.get_param("~use_contours", True)
+    use_contours = rospy.get_param("~use_contours", False)
     rospy.loginfo("map2gazebo running")
     MapConverter(map_topic, threshold=occupied_thresh, height=box_height, use_contours=use_contours)
 
